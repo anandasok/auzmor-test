7b2d547b1a95bb9691ba11a88f67ed2b3a4054ad
Anandraj <anandasok5@gmail.com>
2018-02-26 11:34:26 +0530

    AUZMOR: initial commit

---
+++ crypto-platforms/src/components/FooterCol3.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class FooterCol3 extends Component {
+    render() {
+        return(
+        	<div className="hiring-links">
+                <h3><a href="/hiring">Hiring at Dribbble</a></h3>
+                <ul>
+                    <li><a href="/jobs/info">Post a job</a></li>
+                    <li><a href="/scout/info">Scout designers</a></li>
+                    <li><a href="/teams/info">Add your team</a></li>
+                    <li><a className="external" href="https://crew.co/?utm_source=dribbble&amp;utm_content=dribbble-scout-nav">Crew: Hire freelancers</a></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default FooterCol3;
---
+++ crypto-platforms/src/components/FooterCol4.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+
+class FooterCol4 extends Component {
+    render() {
+        return(
+        	<div className="job-links">
+                <h3><a href="/jobs">Jobs for Designers</a></h3>
+                <ol className="jobs-list">
+
+                    <li className="group">
+                        <a className="item-team" rel="tipsy" href="/reaktor" original-title="Visit the team’s profile">
+                            <img src="https://cdn.dribbble.com/users/970414/avatars/normal/95bc2aa1133fe54e37e37027c4966b82.png?1444704634" alt="95bc2aa1133fe54e37e37027c4966b82" />
+                        </a>
+                        <a className="item-link group" data-url="https://www.reaktor.com/careers/product-designer-for-reaktor-new-york/" rel="nofollow" href="https://www.reaktor.com/careers/product-designer-for-reaktor-new-york/">
+                        <span className="item-info">
+                            <span className="item-title">Reaktor</span> <br />
+                            <span className="item-desc">Lead Product Designer</span>
+                        </span>
+                        </a>
+                    </li>
+
+                    <li className="group">
+                        <a className="item-team" rel="tipsy" href="/basicagency" original-title="Visit the team’s profile">
+                            <img src="https://cdn.dribbble.com/users/161791/avatars/normal/basic-logo-square-01.jpg?1352735445" alt="Basic logo square 01" />
+                        </a>
+                        <a className="item-link group" data-url="https://basicagency.com/careers/creative-director" rel="nofollow" href="https://basicagency.com/careers/creative-director">
+                            <span className="item-info">
+                                <span className="item-title">BASIC</span><br />
+                                <span className="item-desc">Creative Director</span>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li className="group">
+                        <a className="item-team" rel="tipsy" href="/causevox" original-title="Visit the team’s profile">
+                            <img src="https://cdn.dribbble.com/users/871082/avatars/normal/36964d0f7c6a7bac5d1f417ee412018d.png?1486765885" alt="36964d0f7c6a7bac5d1f417ee412018d" />
+                        </a>
+                        <a className="item-link group" data-url="https://causevox.workable.com/jobs/181363" rel="nofollow" href="https://causevox.workable.com/jobs/181363">
+                            <span className="item-info">
+                                <span className="item-title">CauseVox</span> <br />
+                                <span className="item-desc">Front-End Web Developer</span>
+                            </span>
+                        </a>
+                    </li>
+
+                </ol>
+
+            </div>
+
+        )
+    }
+}
+
+export default FooterCol4;
+
+
+
+// <li className="group">
+//                         <a className="item-team" rel="tipsy" href="/basicagency" original-title="Visit the team’s profile">
+//                             <img src="https://cdn.dribbble.com/users/161791/avatars/normal/basic-logo-square-01.jpg?1352735445" alt="Basic logo square 01" />
+//                         </a>
+
+//                         <a className="job-edit" rel="tipsy" data-creator-id="161791" href="/account/jobs/14356/edit" original-title="Edit this job">
+//                             <img src="https://cdn.dribbble.com/assets/icon-edit-button-8d0f2493aa649d9e18d5cf27ee32c93aaa6488ddda85b481f6ca341b85fde845.png" alt="Icon edit button" />
+//                         </a>
+//                         <a className="item-link group" data-url="https://basicagency.com/careers/creative-director" rel="nofollow" href="https://basicagency.com/careers/creative-director">
+//                             <span className="item-info">
+//                                 <span className="item-title">BASIC</span>
+//                                 <span className="item-desc">Creative Director</span>
+//                             </span>
+//                             <span className="item-meta">Mountain View, California</span>
+//                         </a>
+//                     </li>
+
+//                     <li className="group">
+//                         <a className="item-team" rel="tipsy" href="/causevox" original-title="Visit the team’s profile">
+//                             <img src="https://cdn.dribbble.com/users/871082/avatars/normal/36964d0f7c6a7bac5d1f417ee412018d.png?1486765885" alt="36964d0f7c6a7bac5d1f417ee412018d" />
+//                         </a>
+
+//                         <a className="job-edit" rel="tipsy" data-creator-id="871082" href="/account/jobs/14303/edit" original-title="Edit this job">
+//                             <img src="https://cdn.dribbble.com/assets/icon-edit-button-8d0f2493aa649d9e18d5cf27ee32c93aaa6488ddda85b481f6ca341b85fde845.png" alt="Icon edit button" />
+//                         </a>
+//                         <a className="item-link group" data-url="https://causevox.workable.com/jobs/181363" rel="nofollow" href="https://causevox.workable.com/jobs/181363">
+//                             <span class="item-info">
+//                                 <span class="item-title">CauseVox</span>
+//                                 <span class="item-desc">Front-End Web Developer</span>
+//                             </span>
+//                             <span class="item-meta">Anywhere</span>
+//                         </a>
+//                     </li>
