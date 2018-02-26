173c8964755dfcb3538e0f519f65d4b3d9bbb550
Anandraj <anandasok5@gmail.com>
2018-02-26 11:51:34 +0530

    AUZMOR: changing smart component to dump component

--- crypto-platforms/src/components/Footer.js
+++ crypto-platforms/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 // Custom components
 import FooterCol1 from './FooterCol1';
@@ -7,37 +7,35 @@ import FooterCol3 from './FooterCol3';
 import FooterCol4 from './FooterCol4';
 import FooterCol5 from './FooterCol5';
 
-class Footer extends Component {
-    render() {
-        return(
-            <div id="footer">
-                <div id="footer-inner">
-                    <div className="container-fluid">
-                        <div className="col-md-3 footer-col">
-                            <FooterCol1 />
-                        </div>
-
-                        <div className="col-md-2 footer-col">
-                            <FooterCol2 />
-                        </div>
-
-                        <div className="col-md-2 footer-col">
-                            <FooterCol3 />
-                        </div>
-
-                        <div className="col-md-2 footer-col">
-                            <FooterCol4 />
-                        </div>
-
-                        <div className="col-md-3 footer-col">
-                            <FooterCol5 />
-                        </div>
-
+const Footer = () => {
+    return(
+        <div id="footer">
+            <div id="footer-inner">
+                <div className="container-fluid">
+                    <div className="col-md-3 footer-col">
+                        <FooterCol1 />
                     </div>
+
+                    <div className="col-md-2 footer-col">
+                        <FooterCol2 />
+                    </div>
+
+                    <div className="col-md-2 footer-col">
+                        <FooterCol3 />
+                    </div>
+
+                    <div className="col-md-2 footer-col">
+                        <FooterCol4 />
+                    </div>
+
+                    <div className="col-md-3 footer-col">
+                        <FooterCol5 />
+                    </div>
+
                 </div>
             </div>
+        </div>
     )
-    }
 }
 
 export default Footer;
--- crypto-platforms/src/components/SlatHeader.js
+++ crypto-platforms/src/components/SlatHeader.js
@@ -1,49 +1,45 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-// Custom components
+const SlatHeader = () => {
+	return(
+		<div className="container">
+			<div className="row">
+				<div className="col-md-2">
 
-class SlatHeader extends Component {
-    render() {
-        return(
-        	<div className="container">
-        		<div className="row">
-        			<div className="col-md-2">
-                        
-                    </div>
+				</div>
 
-                    <div className="col-md-8">
-                        <div className="slat-header">
+				<div className="col-md-8">
+					<div className="slat-header">
 
-							<a className="url hoverable" rel="contact" title="Ron Design" href="/RonDesignLab">
-								<picture>
-									<img className="photo" src="https://cdn.dribbble.com/users/834691/avatars/small/9006fd85d117c7d3bc8a9e949dc01fcb.png?1430481893" />
-								</picture>
-							</a>
-			    			<h1>Animation Cryptocurrency Marketplace (Light)</h1>
+						<a className="url hoverable" rel="contact" title="Ron Design" href="/RonDesignLab">
+							<picture>
+								<img className="photo" src="https://cdn.dribbble.com/users/834691/avatars/small/9006fd85d117c7d3bc8a9e949dc01fcb.png?1430481893" />
+							</picture>
+						</a>
+						<h1>Animation Cryptocurrency Marketplace (Light)</h1>
 
-			    			<h2 className="shot-byline">
-				      			<span className="attribution is-team">
-					        		<span className="shot-byline-user">
-					          			by <a className="url hoverable" rel="contact" href="/RonDesignLab">Ron Design</a>
-				          			</span>
-				      			</span>
+						<h2 className="shot-byline">
+							<span className="attribution is-team">
+								<span className="shot-byline-user">
+									by <a className="url hoverable" rel="contact" href="/RonDesignLab">Ron Design</a>
+								</span>
+							</span>
 
-						        <span className="shot-byline-date">
-						          on <a href="/shots?date=2017-11-28">Nov 28, 2017</a>
-						        </span>
-			    			</h2>
-			    			
-			  			</div>
-                    </div>
+							<span className="shot-byline-date">
+							  on <a href="/shots?date=2017-11-28">Nov 28, 2017</a>
+							</span>
+						</h2>
 
-                    <div className="col-md-2">
-                        
-                    </div>
-        		</div>
-        	</div>
-           
-        )
-    }
+					</div>
+				</div>
+
+				<div className="col-md-2">
+
+				</div>
+			</div>
+		</div>
+
+	)
 }
 
 export default SlatHeader;
